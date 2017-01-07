7622fb0e446ccc1b5388340970a62e7001268b4e
ajajfkp <aijaz.fkp@gmail.com>
2017-01-08 03:08:02 +0530

    add

--- mybooksrus/application/views/home/about.php
+++ mybooksrus/application/views/home/about.php
@@ -1,2 +1,26 @@
-<div class="container" style="margin-top: 60px;">
-	<h1>About</h1>
+<div id="aboutwrap">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-8 col-lg-offset-2">
+				<h2>We Are Storytellers.<br>
+					Brands Are Our Subjects.
+				</h2>
+			</div>
+		</div><!-- row -->
+	</div><!-- /container -->
+</div>
+<div id="chartwrap">
+	<div class="container">
+	  <div class="row mt">
+		  <div class="col-lg-8 chart hidden-sm hidden-xs">
+			<img class="img-responsive" src="<?php echo base_url(); ?>assets/images/chart.png" alt="">
+		  </div><!-- col-lg-8 -->
+		  
+		  <div class="col-lg-4">
+			<h1>About Link</h1>
+			<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p> 
+			<p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software</p>
+		  </div><!-- col-lg-4 -->
+	  </div><!-- /row -->
+	</div><!-- /.container -->
+</div>
