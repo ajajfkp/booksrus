f960b680a3c7c951c8003f4db4e16ef5722445ba
ajajfkp <aijaz.fkp@gmail.com>
2017-01-25 04:51:11 +0530

    db changes

--- mybooksrus/database/update_db.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `books_transaction`
-DROP `price`,
-COMMENT=''; -- 0.090 s
-
-ALTER TABLE `books`
-ADD `image` varchar(255) COLLATE 'latin1_swedish_ci' NOT NULL AFTER `size`,
-COMMENT=''; -- 0.260 s
